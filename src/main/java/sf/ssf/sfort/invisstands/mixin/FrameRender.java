8bb00bed415407abadd1d8e3fdbfa5e7ed80e8ad
SFort <github@ssf.tf>
2021-06-23 03:25:46 +0200

    fix

--- src/main/java/sf/ssf/sfort/invisstands/mixin/FrameRender.java
+++ src/main/java/sf/ssf/sfort/invisstands/mixin/FrameRender.java
@@ -1,28 +1,25 @@
 package sf.ssf.sfort.invisstands.mixin;
 
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.entity.ArmorStandEntityRenderer;
-import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.client.render.entity.model.ArmorStandArmorEntityModel;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.item.ItemStack;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-@Mixin(ArmorStandEntityRenderer.class)
-public abstract class FrameRender extends LivingEntityRenderer<ArmorStandEntity, ArmorStandArmorEntityModel> {
-	public FrameRender(EntityRendererFactory.Context ctx, ArmorStandArmorEntityModel model, float shadowRadius) {
-		super(ctx, model, shadowRadius);
-	}
-	@Nullable
-	public RenderLayer getRenderLayer(ArmorStandEntity entity, boolean showBody, boolean translucent, boolean showOutline) {
-		if (!ssf$hasItems(entity))
-			showBody = true;
-		else if (!showBody)
-			translucent = false;
-
-		return super.getRenderLayer(entity, showBody, translucent, showOutline);
+@Mixin(LivingEntityRenderer.class)
+public abstract class FrameRender<T extends LivingEntity> {
+	@ModifyArgs(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;getRenderLayer(Lnet/minecraft/entity/LivingEntity;ZZZ)Lnet/minecraft/client/render/RenderLayer;"))
+	public void getRenderLayer(Args args) {
+		LivingEntity entity = args.get(0);
+		if (entity instanceof ArmorStandEntity)
+		if (!ssf$hasItems((ArmorStandEntity) entity))
+			args.set(1, true);
+		else if (args.get(1).equals(false))
+			args.set(2, false);
 	}
 	private boolean ssf$hasItems(ArmorStandEntity entity) {
 		for(ItemStack item : entity.getArmorItems())
