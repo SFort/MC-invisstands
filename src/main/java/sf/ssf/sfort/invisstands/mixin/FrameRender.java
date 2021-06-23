bf056d96f85e8bcfe2189ff5960801c1ac026c3e
SFort <github@ssf.tf>
2021-06-23 02:26:59 +0200

    1.16.5 backport

--- src/main/java/sf/ssf/sfort/invisstands/mixin/FrameRender.java
+++ src/main/java/sf/ssf/sfort/invisstands/mixin/FrameRender.java
@@ -2,7 +2,7 @@ package sf.ssf.sfort.invisstands.mixin;
 
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.ArmorStandEntityRenderer;
-import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.model.ArmorStandArmorEntityModel;
 import net.minecraft.entity.decoration.ArmorStandEntity;
@@ -12,9 +12,11 @@ import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(ArmorStandEntityRenderer.class)
 public abstract class FrameRender extends LivingEntityRenderer<ArmorStandEntity, ArmorStandArmorEntityModel> {
-	public FrameRender(EntityRendererFactory.Context ctx, ArmorStandArmorEntityModel model, float shadowRadius) {
-		super(ctx, model, shadowRadius);
+
+	public FrameRender(EntityRenderDispatcher dispatcher, ArmorStandArmorEntityModel model, float shadowRadius) {
+		super(dispatcher, model, shadowRadius);
 	}
+
 	@Nullable
 	public RenderLayer getRenderLayer(ArmorStandEntity entity, boolean showBody, boolean translucent, boolean showOutline) {
 		if (!ssf$hasItems(entity))
